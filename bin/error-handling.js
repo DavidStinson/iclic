d4f48b471af2e7ebffd1d84c38dc507a114cab26
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2022-08-12 21:05:21 -0500

    build: reset bin dir

--- bin/error-handling.js
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-const log = console.log;
-const cErr = chalk.bold.red;
-function main(data) {
-    console.dir(data);
-}
-export { main };
--- bin/validation.js
+++
@@ -1,5 +0,0 @@
-function checkCurrentShellZSH(currentShell, zshLoc) {
-    const currentShellIsZSH = ((zshLoc === currentShell) && zshLoc.toLowerCase().includes("zsh"));
-    return currentShellIsZSH;
-}
-export { checkCurrentShellZSH, };
